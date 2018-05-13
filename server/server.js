9ec603de72bae9ccbbfce950759e39aadf8ec2fb
Steve <steve.rosenberry@gmail.com>
2018-05-13 13:10:18 -0400

    Add GET Todos by ID REST API

--- server/server.js
+++ server/server.js
@@ -5,12 +5,16 @@ var {mongoose}    = require( './db/mongoose.js' );
 var {Todo}        = require( './models/todo.js' );
 var {User}        = require( './models/user.js' );
 
+
+const {ObjectID} = require( 'mongodb' );
+
 var app = express();
 
 app.use( bodyParser.json() );
 
 
 app.post( '/todos', ( req, res ) => {
+    
     console.log( req.body );
     
     var newTodo = new Todo( {
@@ -29,6 +33,7 @@ app.post( '/todos', ( req, res ) => {
 
 
 app.get( '/todos', ( req, res ) => {
+    
     Todo.find().then( (todos) => {
         res.status( 200 ).send( {todos} ); 
     }, (err) => {
@@ -37,6 +42,37 @@ app.get( '/todos', ( req, res ) => {
     
 });
 
+
+app.get( '/todos/:id', ( req, res ) => {
+    
+    var todoId = req.params.id;
+    
+//    if( !ObjectID.isValid( todoId ) )
+//    {
+//        sendError( res, 400, `todoId[${todoId}] is invalid.` );
+//        return;
+//    }
+    
+    Todo.findById( todoId ).then( (todo) => {
+        if( !todo )
+        {
+            sendError( res, 404, `Todo not found for todoId[${todoId}].` );
+            return;
+        }
+        res.status( 200 ).send( {todo} ); 
+    }, (err) => {
+        sendError( res, 500, `Internal error finding todo for id[${todoId}]` );
+        return;
+    });
+    
+});
+
+function sendError( res, code, message )
+{
+    console.log( message );
+    res.status( code ).send( message );
+    return;
+}
 
 //
 //app.get( '/users', ( req, res ) => {
