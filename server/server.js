e576cd3189f9f248ae89971b6722be3f12843c11
Steve <steve.rosenberry@gmail.com>
2018-05-13 14:14:33 -0400

    Prepare for Heroku deployment using mLab MongoDB

--- server/server.js
+++ server/server.js
@@ -82,7 +82,7 @@ function sendError( res, code, message )
 //});
 //
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen( port, () => {
     console.log( `Started on port ${port}` );
